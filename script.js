ded923fae9b357981940c99b8c10ca6bc5b7f63f
Ashish Rokade <ashishrokade944@gmail.com>
2026-09-14 18:47:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-//your JS code here. If required.
 const buttons = document.querySelectorAll(".btn");
-cont stopButton = dodument.querSelectorAll(".stop");
+const stopButton = document.querySelector(".stop");
 
 let currentAudio = null;
-buttons.forEach(function (button){
-	button.addEventListner("click", function() {
-		if(currentAudio){
-			currentAudio.pause();
-			currentAudio.currentTime = 0;
-		}
-		const soundName = this.getAttribute("data-sound");
 
-		currentAudio = new Audio("./sounds/" + soundName + ".mp3");
-		currentAudio.play();
-	});
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+
+        const soundName = this.getAttribute("data-sound");
+
+        currentAudio = new Audio("./sounds/" + soundName + ".mp3");
+        currentAudio.play();
+    });
 });
 
-stopButton.addEventListener("click", function(){
-	if(currentAdio){
-		currentAudio.pause();
-		currentAudio.currentTime = 0;
-	}
+stopButton.addEventListener("click", function () {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
 });
-
-
-
-
-
-
-
